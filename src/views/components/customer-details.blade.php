eb81fe726ddf5eefa149c73c2b1670b30e49475e
Nitish Kaundil <nitishk879@gmail.com>
2021-05-25 20:43:07 +0530

    Customer Details -Dummy values has been removed.

--- src/views/components/customer-details.blade.php
+++ src/views/components/customer-details.blade.php
@@ -8,11 +8,11 @@
         <div class='form-row payzone-form-section'>
             <div class="col-md-12 mb-3">
                 <label for='CustomerName'>CustomerName</label>
-                <input type="text" id="CustomerName" class="form-control form-control-sm" name="CustomerName" value="Geoff Wayne" />
+                <input type="text" id="CustomerName" class="form-control form-control-sm" name="CustomerName" value="{{ old("CustomerName") ?? '' }}" />
             </div>
             <div class="col-md-12 mb-3">
                 <label for='Address1'>Address1</label>
-                <input type="text" id="Address1" class="form-control form-control-sm" name="Address1" value="113 Glendower Road" />
+                <input type="text" id="Address1" class="form-control form-control-sm" name="Address1" value="{{ old("Address1") ?? '' }}" />
             </div>
             <div class="col-md-12 mb-3">
                 <label for='Address2'>Address2</label>
@@ -20,19 +20,19 @@
             </div>
             <div class="col-md-12 mb-3">
                 <label for='City'>City</label>
-                <input type="text" id="City" class="form-control form-control-sm" name="City" value="Birmingham" />
+                <input type="text" id="City" class="form-control form-control-sm" name="City" value="{{ old("City") ?? '' }}" />
             </div>
             <div class="col-md-12 mb-3">
                 <label for='State'>State</label>
-                <input type="text" id="State" class="form-control form-control-sm" name="State" value="West Midlands" />
+                <input type="text" id="State" class="form-control form-control-sm" name="State" value="{{ old("State") ?? '' }}" />
             </div>
             <div class="col-md-12 mb-3">
                 <label for='EmailAddress'>EmailAddress</label>
-                <input type="text" id="EmailAddress" class="form-control form-control-sm" name="EmailAddress" value="contact@geoffwayne.com" />
+                <input type="text" id="EmailAddress" class="form-control form-control-sm" name="EmailAddress" value="{{ old("EmailAddress") ?? '' }}" />
             </div>
             <div class="col-md-12 mb-3">
                 <label for='PostCode'>PostCode</label>
-                <input type="text" id="PostCode" class="form-control form-control-sm" name="PostCode" value="B42 1SX" />
+                <input type="text" id="PostCode" class="form-control form-control-sm" name="PostCode" value="{{ old("PostCode") ?? '' }}" />
             </div>
             <div class="col-md-12 mb-3">
                 <label for='Country'>Country</label>
